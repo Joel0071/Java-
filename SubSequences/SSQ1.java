e47c038cb621041cb357e16c2abfac322b9ed07b
Joel Nadar <104148617+Joel0071@users.noreply.github.com>
2023-03-16 16:10:44 +0530

    Create SSQ1.java

---
+++ SubSequences/SSQ1.java
@@ -0,0 +1,15 @@
+public static ArrayList<String> returnSSQ1(String s){
+        ArrayList<String> ans=new ArrayList<>();
+        if(s.length()==0){
+            ans.add(" ");
+            return ans;
+        }
+        char current=s.charAt(0);
+        ArrayList<String> small=new ArrayList<>();
+        small=returnSSQ(s.substring(1));
+        for(String ss:small){
+            ans.add(ss);
+            ans.add(current+ss);
+        }
+        return ans;
+    }
